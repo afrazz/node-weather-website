44d0796acb9a3d8948aaef90a6a4cb209ea1d057
afras <muhammedafras0@gmail.com>
2021-01-08 20:57:00 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 const forcast = require('./utils/forcast');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -72,6 +74,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on 3000');
+app.listen(port, () => {
+  console.log('Server is up on' + port);
 });
